1e0160bfb27a83b17b5abac88d1ab705c18a78fb
WhileR0ck <stepvadimus69@gmail.com>
2026-03-26 19:04:52 +0300

    help

--- app/src/main/java/org/example/backend.java
+++ app/src/main/java/org/example/backend.java
@@ -1,5 +1,32 @@
 package org.example;
 
-public class BeckEnd {
+public class backend {
+    // хорошая логика для калькулька что всё робит пуперски
+    public float calc(float first, float second, char ch){
+        float res = first;
+        switch (ch) {
+            case '+':
+                 res += second;
+                 break;
+             case '-':
+                 res -= second;
+                 break;
+             case '*':
+                 res *= second;
+                 break;
+             case '/':
+                try {
+                    res /= second;
+                } catch (ArithmeticException e) {
+                    System.out.println("You can`t divide by 0");
+                   System.out.println(e);
+                }
+                break;
+            default:
+                System.out.println("Какой долбоеб не то жмет");
+                break;
+         }
 
+         return res;
+    }
 }
--- app/src/main/java/org/example/ui.java
+++ app/src/main/java/org/example/ui.java
@@ -1,5 +1,5 @@
 package org.example;
 
-public class UI {
+public class ui {
 
 }
